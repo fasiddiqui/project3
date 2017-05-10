7ee11a5194de65d71de0e6be641ee92d6821b857
awhelan-school <awhelan@ucdavis.edu>
2017-05-09 22:46:45 -0700

    Add files via upload

---
+++ libuthread/sem.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "queue.h"
+#include "sem.h"
+#include "thread.h"
+
+sem_t sem_create(size_t count)
+{
+	return NULL;
+}
+
+int sem_destroy(sem_t sem)
+{
+	return 0;
+}
+
+int sem_down(sem_t sem)
+{
+	return 0;
+}
+
+int sem_up(sem_t sem)
+{
+	return 0;
+}
+
