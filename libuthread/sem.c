386483e528c2b3b307f6f23dce8af93b36075310
awhelan-school <awhelan@ucdavis.edu>
2017-05-15 22:48:50 -0700

    Add files via upload

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -4,7 +4,7 @@
 
 #include "queue.h"
 #include "sem.h"
-#include "thread.h"
+
 
 struct semaphore 
 {
@@ -12,6 +12,10 @@ struct semaphore
   queue_t List; // list of thread IDs
 };
 
+
+// GLOBAL VARIABLES //
+
+pthread_t tid;
 
 sem_t sem_create(size_t count)
 {
@@ -47,30 +51,38 @@ int sem_destroy(sem_t sem)
 
 int sem_down(sem_t sem)
 {
-	enter_critical_section();
-  	//Error_Checking(UnAllocated Object)
-  	if(sem == NULL)
-    		return(-1);
-  	else if(sem->value > 0){
-    		sem->value -= 1;
-    		exit_critical_section();
-	}
-	
-  	return 0;
-	
+  enter_critical_section();
+  
+  //Error_Checking(UnAllocated Object)
+  if(sem == NULL)
+    return(-1);
+  
+  if(sem->value > 0)
+    sem->value -= 1;
+  else{
+    tid = pthread_self();   
+    queue_enqueue(sem->List, tid);  
+    thread_block();
+  }
+  
+  exit_critical_section();
+  
+  return 0;
 }
+
 int sem_up(sem_t sem)
 {
-	
-	if(queue_length(sem->List)){
-		enter_critical_section();
-    	}//QUEUE_NOT_EMPTY
-  	else{
-    		sem->value += 1;
-		exit_critical_section();
-  	}//increment
-    		
-	
+  enter_critical_section();
+  
+  if(queue_length(sem->List)){   
+    thread_unblock(tid);//wakeup this thread
+    queue_dequeue(sem->List, (void*)tid);//remove thread via its ID   
+  }//QUEUE_NOT_EMPTY
+  else{
+    sem->value += 1;
+  }//increment
+    
+  exit_critical_section();
   return 0;
 }
 
